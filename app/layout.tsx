733ca48b867631ef77ce66cceed7fc116baf31fd
Zachary MacArthur <91560582+Zachary-MacArthur@users.noreply.github.com>
2024-12-17 18:54:51 -0500

    1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,40 @@
-import type { Metadata } from "next";
-import { Figtree } from "next/font/google";
-import "./globals.css";
-import Sidebar from '@/components/Sidebar'
-import SupabaseProvider from "@/providers/SupabaseProvider";
+import "./globals.css"
+import { Figtree } from "next/font/google"
 
-const font = Figtree({
-  subsets: ["latin"],
-});
+import Sidebar from "../components/Sidebar"
+import SupabaseProvider from "./providers/SupabaseProvider"
+import UserProvider from "./providers/UserProvider"
+import ModalProvider from "./providers/ModalProvider"
+import ToasterProvider from "./providers/ToastProvider"
+import getSongsByUserId from "@/actions/getSongsByUserId"
+import Player from "@/components/Player"
+import getActiveProductsWithPrices from "@/actions/getActiveProductsWithPrices"
 
+const figtree = Figtree({ subsets: ["latin"] })
 
-export const metadata: Metadata = {
-  title: "self hosted media app",
-  description: "listen to music",
-};
+export const metadata = {
+  title: "Spotify clone",
+  description: "Listen to music!",
+}
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export const revalidate = 0
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const userSongs = await getSongsByUserId()
+  const products = await getActiveProductsWithPrices()
+
   return (
     <html lang="en">
-      <body
-        className={`${font.className} ${font.className} antialiased`}
-      >
+      <body className={figtree.className}>
+        <ToasterProvider />
         <SupabaseProvider>
-          <Sidebar>
-            {children}
-          </Sidebar>
+          <UserProvider>
+            <ModalProvider products={products} />
+            <Sidebar songs={userSongs}>{children}</Sidebar>
+            <Player />
+          </UserProvider>
         </SupabaseProvider>
       </body>
     </html>
-  );
+  )
 }
